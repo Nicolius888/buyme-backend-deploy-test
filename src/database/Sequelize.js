9f52a3b55768ddd38ea4bda80eb3447c4670f09d
nicolius888 <nicola.edgardo@gmail.com>
2022-02-12 21:15:47 -0300

    deployment running-update to last backend version

--- src/database/Sequelize.js
+++ src/database/Sequelize.js
@@ -2,19 +2,16 @@
 
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DATABASE_URL } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
-  // ecommerce es el nombre de la base de datos local
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  freezeTableName: true, //prevent sequelize from pluralizing table names
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce`,
+  {
+    // ecommerce es el nombre de la base de datos local
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    freezeTableName: true, //prevent sequelize from pluralizing table names
+  }
+);
 
 module.exports = sequelize;
